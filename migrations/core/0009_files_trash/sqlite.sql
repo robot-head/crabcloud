84463f401d61f2eabde50370102a02aae3e3a8f2
Matthew Stone <matthew.d.stone@gmail.com>
2026-05-16 20:28:34 -0700

    trash: 0009_files_trash migration triplet

---
+++ migrations/core/0009_files_trash/sqlite.sql
@@ -0,0 +1,13 @@
+CREATE TABLE oc_files_trash (
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    "user"         VARCHAR(64)  NOT NULL,
+    basename       VARCHAR(255) NOT NULL,
+    suffix         VARCHAR(32)  NOT NULL,
+    location       VARCHAR(512) NOT NULL,
+    deleted_at     BIGINT       NOT NULL,
+    type           VARCHAR(16)  NOT NULL,
+    fileid_legacy  BIGINT       NULL
+);
+
+CREATE INDEX        idx_trash_user_deleted ON oc_files_trash ("user", deleted_at);
+CREATE UNIQUE INDEX idx_trash_user_name    ON oc_files_trash ("user", basename, suffix);
